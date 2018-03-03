6916571a14b4a53ee7ca65f3e049196904dbf3a9
Jenny Chen <xc14@illinois.edu>
2018-03-02 23:11:38 -0600

    combined queue to home html 💯

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,17 +1,20 @@
-var stack = document.getElementById("stackAnimation"),
-    defaultBG = document.getElementById("defaultPicture"),
+var defaultBG = document.getElementById("defaultPicture"),
+
+    stack = document.getElementById("stackAnimation"),
     stack_input_group = document.getElementById("stackInputs"),
+
     linkedList = document.getElementById("linkedListAnimation"),
+
     queue = document.getElementById("queueAnimation"),
+    queue_input_group = document.getElementById("queueInputs"),
 
     stack_button = document.getElementById("stackButton"),
     linkedList_button = document.getElementById("linkedListButton"),
     queue_button = document.getElementById("queueButton"),
     container = document.getElementById("container");
 
-var thingsToHide = [stack, defaultBG, stack_input_group, linkedList, queue];
+var thingsToHide = [stack, defaultBG, stack_input_group, linkedList, queue, queue_input_group];
 var thingsToClear = [stack];
-var count = 0;
 
 function clear() {
   for (var i = 0; i < thingsToHide.length; i++) {
@@ -35,5 +38,9 @@ linkedList_button.onclick = () => {
 
 queue_button.onclick = () => {
   clear();
+  queue_input_group.style.display = "flex";
   queue.style.display = "inline-flex";
+  // functions below are defined in queue.js
+  clearQueue();
+  drawQueue();
 }
--- JavaScript/queue.js
+++ JavaScript/queue.js
@@ -4,14 +4,8 @@
 /*
 q.enqueue()
 q.dequeue()
-q.isempty()
 */
 
-//queue structure coords and params
-    // left_x = 200;
-    // right_x = 1000;
-    // upper_y = 100;
-    // lower_y = 200;
 const q_cx = 400,
     q_cy = 200,
     q_struct_width = 480,
@@ -22,11 +16,6 @@ const q_cx = 400,
     RIGHT = q_cx + q_struct_width / 2,
     UPPER = q_cy - q_struct_height / 2,
     LOWER = q_cy + q_struct_height / 2,
-// //output bowl params
-//     output_height = 2.1 * q_struct_height,
-//     output_width = 1.8 * q_struct_height,
-//     output_cx = LEFT / 2,
-//     output_cy = q_cy,
 //data_elem params
     data_elem_width = 90,
     data_elem_height = 90,
@@ -34,8 +23,8 @@ const q_cx = 400,
     data_l_cy = q_cy,
     data_output_x = RIGHT + data_elem_width / 2,
 //labels
-    label_font = "comic sans ms",
-    label_size = 30,
+    q_label_font = "comic sans ms",
+    q_label_size = 30,
     elem_label_size = 23,
 //colors
     q_frame_color = '#7b2d26',
@@ -45,98 +34,72 @@ const q_cx = 400,
     data_queue_gap = 2,
     data_max = 4,
 //data name color arr
-    data_stroke_width = 5,
     name_arr = ['#ffe1d4', '#e0f6f5', '#f5f5f5'],//const?
     fill_cr_arr = ['#ab4a43', '#05a8aa', '#aba194'],
     stroke_cr_arr = ['#7b2d26', '#0b7a75', '#634e32'];
-
+data_stroke_width = 5;
 
 //name arr index
 var data_idx = 0;
-var duration = 1000;
+var enqueue_duration = 1000;
 var move_to_cx = 0;
 
 //canvas
-var draw = SVG('drawing').size(900, 600)
-//queue structure
-var upper = draw.line(LEFT, UPPER, RIGHT, UPPER)
-    upper.stroke({ color: q_frame_color, width: q_stroke_width, linecap: 'round' });
+var queue_draw = SVG('queueAnimation').size(0, 0);
 
-var lower = draw.line(LEFT, LOWER, RIGHT, LOWER)
-    lower.stroke({color: q_frame_color, width: q_stroke_width, linecap: 'round'});
-var q_struct_label = draw.plain("Queue")
-        .font({family: label_font, fill: q_frame_color, size: label_size})
-        .cx(q_cx)
-        .cy(LOWER + 20);
-//data elem
-// var rect_elem = draw.rect(data_elem_width, data_elem_height).fill(q_data_color)
-//                     .cx(data_l_cx)
-//                     .cy(data_l_cy);
-//     rect_elem.radius(10, 20);
-//group queue
-var queue = draw.group()
-    queue.add(upper)
-    queue.add(lower)
-    queue.add(q_struct_label)
-// //group output bowl
-// var output = draw.group();
-// //output bowl
-// var output_frame = output.ellipse(output_width, output_height)
-//         .center(output_cx, output_cy).fill({color: empty})
-//         .stroke({color:q_frame_color, width: q_stroke_width});
-// var output_cover = output.rect(output_width - 20, output_height * 0.75)
-//         .fill({color: empty})
-//         .cx((output_cx + LEFT) / 2)
-//         .cy(q_cy);
-// var output_label = output.plain("Output")
-//         .font({family: label_font, fill: q_frame_color, size: label_size})
-//         .cx(output_cx)
-//         .cy(q_cy + output_height /2 + 20);
+function drawQueue() {
+  queue_draw.size(810, 600);
+  //queue structure
+  var upper = queue_draw.line(LEFT, UPPER, RIGHT, UPPER)
+      upper.stroke({ color: q_frame_color, width: q_stroke_width, linecap: 'round' });
 
-//setting background
-var static_bg = draw.group();
-static_bg.add(queue);
-// static_bg.add(output);
-static_bg.back();
+  var lower = queue_draw.line(LEFT, LOWER, RIGHT, LOWER)
+      lower.stroke({color: q_frame_color, width: q_stroke_width, linecap: 'round'});
+  var q_struct_label = queue_draw.plain("Queue")
+          .font({family: q_label_font, fill: q_frame_color, size: q_label_size})
+          .cx(q_cx)
+          .cy(LOWER + 20);
+  //group queue
+  var queue = queue_draw.group()
+      queue.add(upper)
+      queue.add(lower)
+      queue.add(q_struct_label)
 
-// draw the line as the path of the data_elems
-// function draw_line(enqueue, data) {
-//     var lx, ly;
-//     if (enqueue){
-//         lx = data_l_cx;
-//         ly = data_l_cy;
-//     }
-//     else{
-//         lx = output_cx;
-//         ly = output_cy;
-//     }
-//
-//     return draw.path(`M ${data.cx()} ${data.cy()}
-//                 L ${lx} ${ly}`)
-//                 .fill({color: "none"});
-// }
+  //setting background
+  var static_bg = queue_draw.group();
+  static_bg.add(queue);
+  // static_bg.add(output);
+  static_bg.back();
+
+}
+
+// a list to store queue data
+var q_data = []
+
+function clearQueue() {
+  queue_draw.clear();
+  queue_draw.size(0,0);
+  q_data.length = 0;
+}
 
 
 function createData(name) {
-  group = draw.group();
+  group = queue_draw.group();
   data = group.rect(data_elem_width, data_elem_height)
                       .fill(empty)
                       .cx(data_l_cx)
                       .cy(data_l_cy)
                       .radius(20, 20)
                       .stroke({color: empty, width: data_stroke_width});
-  name = group.plain(name).font({fill: empty, family: label_font, size: elem_label_size});
+  name = group.plain(name).font({fill: empty, family: q_label_font, size: elem_label_size});
   name.center(data_l_cx, data_l_cy);
   group.back();
   return group;
 }
 
-
-var q_data = []
-
 function enqueue(){
-    var data_name = input.value;
-    input.value = "";
+    var data_name = queue_input.value;
+    queue_input.value = "";
     var data = createData(data_name);
     // determine where to move to
     if (q_data.length <= data_max) {
@@ -144,10 +107,10 @@ function enqueue(){
     } else {
       move_to_cx = RIGHT - q_data.length * 3 - (data_max + 1/2) * (data_elem_width + data_stroke_width) - data_l_cx;
     }
-    duration = Math.abs(move_to_cx) * 2;
+    enqueue_duration = Math.abs(move_to_cx) * 2;
     data.get(0).animate(500).stroke({color: stroke_cr_arr[data_idx]}).fill(fill_cr_arr[data_idx]);
     data.get(1).animate(500).font({fill: name_arr[data_idx]});
-    data.animate(duration, "<>", 500).x(move_to_cx);
+    data.animate(enqueue_duration, "<>", 500).x(move_to_cx);
     data_idx += 1;
     data_idx %= fill_cr_arr.length;
     q_data.push(data);
@@ -175,25 +138,18 @@ function dequeue(){
         delete data;
     }
 }
-// function bounce(){
-//     if (bounce_checkbox.checked){
-//         animate_property = {duration: 1500, ease: 'bounce'};
-//     }
-//     else{
-//         animate_property = (duration: 800, ease: 'quadInOut');
-//     }
-// }
+
 //buttons
 var enqueue_button = document.getElementById("enqueue"),
     dequeue_button = document.getElementById("dequeue"),
-    input = document.getElementById("data");
+    queue_input = document.getElementById("queueData");
     // bounce_checkbox = document.getElementById("bounce");
 
 enqueue_button.onclick = enqueue;
 dequeue_button.onclick = dequeue;
 // bounce_checkbox.onclick = bounce;
 
-input.addEventListener("keyup", function(event){
+queue_input.addEventListener("keyup", function(event){
     event.preventDefault();
     if (event.keyCode === 13){
         enqueue_button.click();
